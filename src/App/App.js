2fa7b07ab9788431d789d4c178df396be7046eb6
Deanna Stevens <deanna.sofias@gmail.com>
2024-01-02 12:01:31 -0500

    Add fetched data to App mounting

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import React from 'react';
 import { useEffect, useState } from 'react'
+import { getReservations } from '../apiCalls';
 
 function App() {
   const [reservations, setReservations] = useState([])
 
-
+  useEffect(() => {
+    //invoke fetch on mounting
+    getReservations()
+    .then(data => setReservations(data))
+  }, [])
+  
   return (
     <div className="App">
       <h1 className='app-title'>Turing Cafe Reservations</h1>
