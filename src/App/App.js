f6954163a127872f7035c106a870556d96ac0e27
Deanna Stevens <deanna.sofias@gmail.com>
2024-01-02 17:37:14 -0500

    Update Form as a controlled Form rendering new reservations

--- src/App/App.js
+++ src/App/App.js
@@ -6,21 +6,33 @@ import Reservations from '../Reservations/Reservations'
 import Form from '../Form/Form';
 
 function App() {
-  const [reservations, setReservations] = useState([])
+  const [existingReservations, setExistingReservations] = useState([])
+  const [newReservations, setNewReservations] = useState([])
+// two state work around without POST to manually render new rez
   useEffect(() => {
+    try {
     getReservations()
-    .then(data => setReservations(data))
+    .then(data => setExistingReservations(data))
+    } catch (error) {
+      console.error('Error getting reservations', error)
+    }
   }, [])
+
+  const addReservations = newRez => {
+    setNewReservations(existingReservations => {
+      return [...existingReservations, newRez]
+    })
+  }
+
+  const totalReservations = [...existingReservations, ...newReservations]
 
   return (
     <div className="App">
       <h1 className='app-title'>Turing Cafe Reservations</h1>
       <div className='resy-form'>
-        <Form />
+        <Form  addReservations={addReservations}/>
       </div>
-      <div className='resy-container'>
-        <Reservations reservations={reservations} />
-      </div>
+        <Reservations reservations={totalReservations} />
     </div>
   );
 }
