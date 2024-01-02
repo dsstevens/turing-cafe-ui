98ad01d769d2d725ce838fc13a1f35ba6238157d
Deanna Stevens <deanna.sofias@gmail.com>
2024-01-02 12:36:24 -0500

    Render reservations from fetched data

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import React from 'react';
 import { useEffect, useState } from 'react'
-import { getReservations } from '../apiCalls';
+import { getReservations } from '../apiCalls'
+import Reservations from '../Reservations/Reservations';
 
 function App() {
   const [reservations, setReservations] = useState([])
-
   useEffect(() => {
+    // console.log("mounting phase before fetch")
     //invoke fetch on mounting
     getReservations()
     .then(data => setReservations(data))
@@ -16,7 +17,9 @@ function App() {
     <div className="App">
       <h1 className='app-title'>Turing Cafe Reservations</h1>
       <div className='resy-form'></div>
-      <div className='resy-container'></div>
+      <div className='resy-container'>
+        <Reservations reservations={reservations} />
+      </div>
     </div>
   );
 }
