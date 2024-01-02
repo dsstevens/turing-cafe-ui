7ba9800be9e69b6c192c9ac54df8b5a183dcd1a3
Deanna Stevens <deanna.sofias@gmail.com>
2024-01-02 13:38:57 -0500

    Create basic form and rendering

--- src/App/App.js
+++ src/App/App.js
@@ -2,13 +2,12 @@ import './App.css';
 import React from 'react';
 import { useEffect, useState } from 'react'
 import { getReservations } from '../apiCalls'
-import Reservations from '../Reservations/Reservations';
+import Reservations from '../Reservations/Reservations'
+import Form from '../Form/Form';
 
 function App() {
   const [reservations, setReservations] = useState([])
   useEffect(() => {
-    // console.log("mounting phase before fetch")
-    //invoke fetch on mounting
     getReservations()
     .then(data => setReservations(data))
   }, [])
@@ -16,7 +15,9 @@ function App() {
   return (
     <div className="App">
       <h1 className='app-title'>Turing Cafe Reservations</h1>
-      <div className='resy-form'></div>
+      <div className='resy-form'>
+        <Form />
+      </div>
       <div className='resy-container'>
         <Reservations reservations={reservations} />
       </div>
