9c7c85a20394c938fad23e3f01a9ae52cf0bf3b8
Deanna Stevens <deanna.sofias@gmail.com>
2024-01-02 12:21:17 -0500

    Map individual reservation data onto Card

--- src/App/App.js
+++ src/App/App.js
@@ -11,14 +11,12 @@ function App() {
     getReservations()
     .then(data => setReservations(data))
   }, [])
-  
+
   return (
     <div className="App">
       <h1 className='app-title'>Turing Cafe Reservations</h1>
-      <div className='resy-form'>
-      </div>
-      <div className='resy-container'>
-      </div>
+      <div className='resy-form'></div>
+      <div className='resy-container'></div>
     </div>
   );
 }
